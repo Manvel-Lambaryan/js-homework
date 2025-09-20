55f2cf68ea9bd998875c62866b5620c04785921f
Manvel-Lambaryan <manwellllambaryaniii@gmail.com>
2025-09-20 16:08:14 +0400

    Sort

--- Sorted/countingSort/countingSort.js
+++ Sorted/countingSort/countingSort.js
@@ -4,24 +4,19 @@ function countingSort(arr) {
     let min = Math.min(...arr);
     let max = Math.max(...arr);
     let count = new Array(max - min + 1).fill(0);
-
     for (let num of arr) {
         count[num - min]++;
     }
-
     for (let i = 1; i < count.length; i++) {
         count[i] += count[i - 1];
     }
-
     let output = new Array(arr.length);
-
     for (let i = arr.length - 1; i >= 0; i--) {
         let num = arr[i];
         let index = count[num - min] - 1;
-        output[index] = num;
+    output[index] = num;
         count[num - min]--;
     }
-
     return output;
 }
 
